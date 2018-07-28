6faac4338a1e440527117ad290abfbca4a7a8fcb
Qais Patankar <qaisjp@gmail.com>
2018-07-28 21:29:59 +0100

    Add database init

---
+++ pkg/database/postgres.go
@@ -0,0 +1,18 @@
+package database
+
+import (
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq" // pq adapter for sql
+
+	"github.com/multitheftauto/community/pkg/config"
+)
+
+// NewPostgres connects to the database and returns a query generator
+func NewPostgres(cfg config.PostgresConfig) (*sqlx.DB, error) {
+	db, err := sqlx.Connect("postgres", cfg.ConnectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
