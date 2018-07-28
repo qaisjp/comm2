07819ec47c86a417025596c0398c5a0005d6ebf4
Qais Patankar <qaisjp@gmail.com>
2018-07-28 22:25:48 +0100

    Add missing db annotations

--- pkg/models/account.go
+++ pkg/models/account.go
@@ -5,16 +5,16 @@ import (
 )
 
 type Account struct {
-	ID           uint64
-	DateCreated  time.Time
-	DateModified time.Time
+	ID        uint64    `id`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
 
-	Email          string
-	Username       string
-	Slug           string
-	Password       string
-	Level          int
-	Banned         bool
-	Activated      bool
-	FollowingCount int // calculate on fly? feature necessary?
+	Username       string `db:"username" valid:"stringlength(1|255),required"`
+	Password       string `db:"password" valid:"stringlength(5|100),required"`
+	Email          string `db:"email" valid:"email,stringlength(1|254),required"`
+	Slug           string `db:"slug" valid:"stringlength(1|255),required"`
+	Level          int    `db:"level"`
+	Banned         bool   `db:"banned"`
+	Activated      bool   `db:"activated"`
+	FollowingCount int    `db:"following_count"` // calculate on fly? feature necessary?
 }
