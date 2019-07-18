9e6993d5166571752ea6e465f46dee528ec5d736
Qais Patankar <qaisjp@gmail.com>
2019-07-18 18:20:24 +0900

    web: add octicons module

---
+++ website/src/app/octicons/octicons.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { OcticonDirective } from './octicon.directive';
+
+@NgModule({
+  declarations: [OcticonDirective],
+  imports: [],
+  exports: [OcticonDirective],
+})
+export class OcticonsModule { }
