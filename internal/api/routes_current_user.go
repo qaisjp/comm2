0eacce968d6a23be961d131cadc63a02bc73ad12
Qais Patankar <qaisjp@gmail.com>
2020-01-27 21:42:26 +0000

    add current user route

---
+++ internal/api/routes_current_user.go
@@ -0,0 +1,13 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (a *API) getCurrentUser(c *gin.Context) {
+	user := c.MustGet("current_user").(*User)
+	// todo: support extra private info
+	c.JSON(http.StatusOK, user.PublicInfo())
+}
