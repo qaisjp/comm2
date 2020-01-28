90a7e13a8868f1612705b697e54b1c02a2a2463e
Qais Patankar <qaisjp@gmail.com>
2020-01-28 02:17:23 +0000

    add auth module with simple login form

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileModule } from './profile/profile.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { ProfileModule } from './profile/profile.module';
     BrowserModule,
     AppRoutingModule,
     ProfileModule,
+    AuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
