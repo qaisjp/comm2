05a8b93f24d49e02a991f8bf639fd3b51062faad
Qais Patankar <qaisjp@gmail.com>
2020-03-03 18:12:39 +0000

    move baseurl to env

--- website/src/app/auth/auth.service.ts
+++ website/src/app/auth/auth.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  accessToken: string | null = null;
+
   constructor(private http: HttpClient) { }
 
   public login(username: string, password: string) {
-    this.http.post("http://localhost:8080/v1/auth/login", { username, password })
+    this.http.post(`${environment.api.baseurl}/v1/auth/login`, { username, password })
       .subscribe((data) => {
         console.log(data);
       })
