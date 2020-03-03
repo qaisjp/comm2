2e245dab5c79c0b884c93d4c4db3e42c782c8961
Qais Patankar <qaisjp@gmail.com>
2020-03-03 16:30:01 +0000

    add basic login page

--- website/src/app/auth/auth.module.ts
+++ website/src/app/auth/auth.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, LoginPageComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
