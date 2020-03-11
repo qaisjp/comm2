044ea11178984a4491385e148020ad556c0ae1c4
Qais Patankar <qaisjp@gmail.com>
2020-03-11 01:20:09 +0000

    add register interface

--- website/src/app/app-routing.module.ts
+++ website/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginPageComponent } from './auth/login-page/login-page.component';
+import {RegisterPageComponent} from './auth/register-page/register-page.component';
 
 
 const routes: Routes = [
@@ -13,6 +14,11 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginPageComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'register',
+    component: RegisterPageComponent,
     pathMatch: 'full',
   },
 ];
--- website/src/app/auth/auth.module.ts
+++ website/src/app/auth/auth.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { LoginPageComponent } from './login-page/login-page.component';
+import {RouterModule} from '@angular/router';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 
 
 @NgModule({
-  declarations: [LoginComponent, LoginPageComponent],
+  declarations: [LoginComponent, LoginPageComponent, RegisterPageComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    FormsModule,
+    RouterModule,
   ],
   exports: [
     LoginComponent
