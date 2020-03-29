0718dc2df5e086ea0daefb8502a71825d84fae38
Qais Patankar <qaisjp@gmail.com>
2020-03-29 18:51:06 +0100

    initial manage page

--- website/src/app/profile/profile-routing.module.ts
+++ website/src/app/profile/profile-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile.component';
 import {ResourceViewComponent} from '../resource/view/view.component';
+import {ResourceManageComponent} from '../resource/manage/manage.component';
 
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'u/:username/:resource',
     component: ResourceViewComponent,
+  },
+  {
+    path: 'u/:username/:resource/manage',
+    component: ResourceManageComponent,
   }
 ];
 
--- website/src/app/resource/resource.module.ts
+++ website/src/app/resource/resource.module.ts
@@ -5,11 +5,12 @@ import { ResourceCreateComponent } from './create/create.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {MomentModule} from 'ngx-moment';
 import {RouterModule} from '@angular/router';
+import { ResourceManageComponent } from './manage/manage.component';
 
 
 
 @NgModule({
-  declarations: [ResourceViewComponent, ResourceCreateComponent],
+  declarations: [ResourceViewComponent, ResourceCreateComponent, ResourceManageComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
